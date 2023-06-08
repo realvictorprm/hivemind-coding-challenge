1b9c0bfd05a49c63280e56d182a3f70ca3b40fbe
Victor Peter Rouven Müller <mueller.vpr@gmail.com>
2023-06-08 19:41:33 +0200

    Working version

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 val scala3Version = "3.3.0"
-
+val elastic4sVersion = "8.7.0"
 lazy val root = project
   .in(file("."))
   .settings(
@@ -15,7 +15,8 @@ lazy val root = project
       "dev.zio" %% "zio-streams" % "2.0.15",
       "dev.zio" %% "zio-json" % "0.5.0",
       "com.github.pureconfig" %% "pureconfig" % "0.17.4" cross CrossVersion.for3Use2_13,
-      "io.lambdaworks" %% "zio-elasticsearch" % "0.1.0",
-
+      "com.sksamuel.elastic4s" % "elastic4s-core_3" % elastic4sVersion,
+      "com.sksamuel.elastic4s" %% "elastic4s-client-esjava" % elastic4sVersion,
+      "com.sksamuel.elastic4s" %% "elastic4s-effect-zio" % elastic4sVersion,
     )
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,24 @@
+import com.sksamuel.elastic4s.http.JavaClient
+import com.sksamuel.elastic4s.{ElasticClient, ElasticProperties, RequestSuccess}
 import zio.*
 import zio.http.*
-import sttp.client3.httpclient.zio.HttpClientZioBackend
-import zio.elasticsearch.*
 import zio.*
-import zio.elasticsearch.aggregation.ElasticAggregation
-import zio.elasticsearch.request.CreationOutcome
-import zio.elasticsearch.result.SearchAndAggregateResult
 import zio.schema.{DeriveSchema, Schema}
 import zio.stream.{ZSink, ZStream}
 import zio.json.*
 import zio.json.ast.Json
 
-import java.time.{LocalDate, ZoneOffset}
+import java.time.{LocalDate, LocalTime, ZoneOffset}
 import scala.io.Source
 import scala.language.postfixOps
+import com.sksamuel.elastic4s.ElasticDsl.*
+import com.sksamuel.elastic4s.fields.KeywordField
+import com.sksamuel.elastic4s.requests.indexes.IndexRequest
+import com.sksamuel.elastic4s.requests.searches.aggs.DateRangeAggregation
+import com.sksamuel.elastic4s.zio.instances.*
+import com.sksamuel.elastic4s.requests.searches.aggs.*
+import com.sksamuel.elastic4s.requests.searches.aggs.responses.bucket.Terms
+import com.sksamuel.elastic4s.requests.searches.queries.*
 
 // {
 //  "start": "01.01.2010",
@@ -21,76 +26,154 @@ import scala.language.postfixOps
 //  "limit": 2,
 //  "min_number_reviews": 2
 // }
-case class Payload(start: LocalDate, end: LocalDate, limit: Int, min_number_reviews: Int)
+case class InputPayload(
+    start: LocalDate,
+    end: LocalDate,
+    limit: Int,
+    min_number_reviews: Int
+)
 
-object Payload {
-  implicit val decoder: JsonDecoder[Payload] = DeriveJsonDecoder.gen[Payload]
+object InputPayload {
+  implicit val encoder: JsonEncoder[InputPayload] =
+    DeriveJsonEncoder.gen[InputPayload]
+  implicit val decoder: JsonDecoder[InputPayload] =
+    DeriveJsonDecoder.gen[InputPayload]
 }
 
-// {"asin":"","helpful":[],"overall":null,"reviewText":"","reviewerID":"","reviewerName":"","summary":"","unixReviewTime":-1}
-case class Review(asin: String, helpful: List[Int], overall: Float, reviewText: String, reviewerName: String, summary: String, unixReviewTime: Long)
+case class OutputPayload(asin: String, average_rating: Double)
+
+object OutputPayload {
+  implicit val encoder: JsonEncoder[OutputPayload] =
+    DeriveJsonEncoder.gen[OutputPayload]
+  implicit val decoder: JsonDecoder[OutputPayload] =
+    DeriveJsonDecoder.gen[OutputPayload]
+}
+
+case class Review(
+    asin: String,
+    helpful: List[Int],
+    overall: Double,
+    reviewText: String,
+    reviewerName: String,
+    summary: String,
+    unixReviewTime: Long
+)
 
 object Review {
-  implicit val schema: Schema.CaseClass7[_, _, _, _, _, _, _, Review] = DeriveSchema.gen[Review]
+  implicit val encoder: JsonEncoder[Review] = DeriveJsonEncoder.gen[Review]
   implicit val decoder: JsonDecoder[Review] = DeriveJsonDecoder.gen[Review]
-  val (asin, helpful, overall, reviewText, reviewerName, summary, unixReviewTime) = schema.makeAccessors(FieldAccessorBuilder)
 }
 
 object HelloWorld extends ZIOAppDefault {
-  val indexName = IndexName("index")
 
-  def responseError(msg: String) = Response.text(msg).withStatus(Status.InternalServerError)
+  import concurrent.ExecutionContext.Implicits.global
+  import OutputPayload._
 
-  def getEpoch(localDate: LocalDate) = localDate.toEpochDay
+  val indexName = "index"
 
-  def app(): App[Any] =
-    Http.collectZIO[Request] {
-      case req@Method.POST -> Root / "amazon" / "best-rated" =>
-        for {
-          body <- req.body.asString.mapError(it => responseError(it.getMessage))
-          _ = println("extracted body")
-          payload <- ZIO.fromEither(body.fromJson[Payload]).mapError(it => responseError(it))
-          _ = println(payload)
-          agg = ElasticAggregation.termsAggregation("agg", "overall").withSubAgg(ElasticAggregation.("avg", "overall"))
-          _ = println(agg.paramsToJson.toString())
-          req = ElasticRequest
-            .search(indexName, ElasticQuery.range(Review.unixReviewTime).gte(payload.start.toEpochDay).lt(payload.end.toEpochDay))
-            .aggregate(agg)
-          res <- Elasticsearch
-            .execute(req).provide(
-            ElasticExecutor.local,
-            Elasticsearch.layer,
-            HttpClientZioBackend.layer()
-          ).aggregation("asin").map { it => Response.json(it.map(_.toString()).toString()) }
-            .mapError { it => responseError(it.getMessage) }
-        } yield res
-    }
+  def responseError(msg: String) =
+    Response.json(msg).withStatus(Status.InternalServerError)
+
+  def app(client: ElasticClient): App[Any] =
+    Http
+      .collectZIO[Request] {
+        case req @ Method.POST -> Root / "amazon" / "best-rated" =>
+          for {
+            // We need to extract the information from the body first
+            body <- req.body.asString.mapError(it =>
+              responseError(it.getMessage)
+            )
+            payload <- ZIO
+              .fromEither(body.fromJson[InputPayload])
+              .mapError(it => responseError(it))
+            // Use the payload to construct the search-aggregation request
+            req = search(indexName)
+              // Select only those values which are within the requested timeframe.
+              // Yes the way time is handled here could possibly be nicer.
+              .query(
+                RangeQuery("unixReviewTime")
+                  .lte(
+                    payload.end.toEpochSecond(LocalTime.NOON, ZoneOffset.MIN)
+                  )
+                  .gte(
+                    payload.start.toEpochSecond(LocalTime.NOON, ZoneOffset.MIN)
+                  )
+              )
+              .aggs(
+                // We want to group by asin and avg the overall field while ordering descending
+                TermsAggregation("avgOverall")
+                  .field("asin")
+                  .size(payload.limit)
+                  .minDocCount(payload.min_number_reviews)
+                  .subaggs(AvgAggregation("overall").field("overall"))
+                  .order(TermsOrder("overall", false))
+              )
+            // Excecute the search request and extract the resulting avg-overall data to construct the response
+            res <- client
+              .execute(req)
+              .map {
+                case RequestSuccess(_, _, _, res) =>
+                  Response.json(
+                    res.aggs
+                      .result[Terms]("avgOverall")
+                      .buckets
+                      .map { bucket =>
+                        OutputPayload(bucket.key, bucket.avg("overall").value)
+                      }
+                      .toJson
+                  )
+                case rest => responseError(rest.toString)
+              }
+              .mapError { it => responseError(it.getMessage) }
+          } yield res
+      }
+      .mapError { error =>
+        println(error.toString)
+        error
+      }
 
   override val run =
     for {
+      // Begin with reading in the command line argument that provides the path to the sample data
       args <- this.getArgs
-      ingestFileUrl = args.headOption.getOrElse("./sampleData.json")
+      ingestFileUrl = args.headOption.getOrElse("./amazon-reviews.json")
       _ = println(ingestFileUrl)
-      _ <- Elasticsearch.execute(ElasticRequest.createIndex(indexName)).provide(
-        ElasticExecutor.local,
-        Elasticsearch.layer,
-        HttpClientZioBackend.layer()
-      )
-      source <- ZIO.fromAutoCloseable(ZIO.from(Source.fromFile(ingestFileUrl)))
-      lines <- ZStream.fromIterator(source.getLines()).run(ZSink.collectAll)
-      res <- lines.map { line =>
-        ZIO.fromEither(line.fromJson[Review]).map(payload => ElasticRequest.create[Review](indexName, payload))
-      }.collectZIO(identity(_)).flatMap { requests =>
-        val it = requests.toArray[BulkableRequest[_]]
-        Elasticsearch.execute(ElasticRequest.bulk(it: _*)).provide(
-          ElasticExecutor.local,
-          Elasticsearch.layer,
-          HttpClientZioBackend.layer()
+      // Init the elastic client
+      client <- ZIO.fromAutoCloseable(
+        ZIO.from(
+          ElasticClient(JavaClient(ElasticProperties("http://localhost:9200")))
         )
-      }
+      )
+      // We need a fresh index with asin being a keyword field for group-by operations
+      _ <- client.execute(deleteIndex(indexName))
+      _ <- client.execute(
+        createIndex(indexName).mapping(properties(keywordField("asin")))
+      )
+      // Read in the sample data and ingest it into the elasticsearch index as a bulk
+      source <- ZIO.fromAutoCloseable(ZIO.from(Source.fromFile(ingestFileUrl)))
+      // Side Note: The chunk size is something that needs to be tested invidually for the machine.
+      // It definitely is necessary as the ingestion process is otherwise either way too slow or without any chunking
+      // we will crash at some point as we run out of memory.
+      // My number can be quite optimistic considering how long a line can be ;)
+      _ <- ZStream
+        .fromIterator(source.getLines(), maxChunkSize = 50_000)
+        .runForeachChunkScoped { lines =>
+          println("ingesting " + lines.size)
+          lines
+            .map { line =>
+              ZIO
+                .fromEither(line.fromJson[Review])
+                .map(payload => indexInto(indexName).source(payload.toJson))
+            }
+            .collectZIO(identity(_))
+            .flatMap { requests =>
+              val it = requests.toArray
+              client.execute(bulk(it: _*))
+            }
+        }
 
-      _ = println(res)
       _ = println("starting webserver")
-      _ <- Server.serve(app()).provide(Server.defaultWithPort(8080))
+      // Get the webserver running
+      _ <- Server.serve(app(client)).provide(Server.defaultWithPort(8080))
     } yield ()
 }
