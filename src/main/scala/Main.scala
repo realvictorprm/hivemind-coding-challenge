fad0c7d3dc98c5215b7807318f77d56bb3a29745
Victor Peter Rouven Müller <mueller.vpr@gmail.com>
2023-06-08 16:15:00 +0200

    that still aint working but keeping for history

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,10 +3,13 @@ import zio.http.*
 import sttp.client3.httpclient.zio.HttpClientZioBackend
 import zio.elasticsearch.*
 import zio.*
+import zio.elasticsearch.aggregation.ElasticAggregation
 import zio.elasticsearch.request.CreationOutcome
+import zio.elasticsearch.result.SearchAndAggregateResult
 import zio.schema.{DeriveSchema, Schema}
 import zio.stream.{ZSink, ZStream}
 import zio.json.*
+import zio.json.ast.Json
 
 import java.time.{LocalDate, ZoneOffset}
 import scala.io.Source
@@ -18,7 +21,7 @@ import scala.language.postfixOps
 //  "limit": 2,
 //  "min_number_reviews": 2
 // }
-case class Payload(start: LocalDate, end: LocalDate, limit: Int, minNumberReviews: Int)
+case class Payload(start: LocalDate, end: LocalDate, limit: Int, min_number_reviews: Int)
 
 object Payload {
   implicit val decoder: JsonDecoder[Payload] = DeriveJsonDecoder.gen[Payload]
@@ -42,19 +45,23 @@ object HelloWorld extends ZIOAppDefault {
 
   def app(): App[Any] =
     Http.collectZIO[Request] {
-      case req@Method.GET -> Root / "amazon" / "best-rated" =>
+      case req@Method.POST -> Root / "amazon" / "best-rated" =>
         for {
           body <- req.body.asString.mapError(it => responseError(it.getMessage))
+          _ = println("extracted body")
           payload <- ZIO.fromEither(body.fromJson[Payload]).mapError(it => responseError(it))
           _ = println(payload)
+          agg = ElasticAggregation.termsAggregation("agg", "overall").withSubAgg(ElasticAggregation.("avg", "overall"))
+          _ = println(agg.paramsToJson.toString())
+          req = ElasticRequest
+            .search(indexName, ElasticQuery.range(Review.unixReviewTime).gte(payload.start.toEpochDay).lt(payload.end.toEpochDay))
+            .aggregate(agg)
           res <- Elasticsearch
-            .execute(ElasticRequest.search(indexName, ElasticQuery.range(Review.unixReviewTime).gte(payload.start.toEpochDay).lt(payload.end.toEpochDay))
-              .aggregate(ElasticAggregation.termsAggregation("aggr", "asin")))
-            .provideLayer(Elasticsearch.layer)
-            .provideLayer(ElasticExecutor.local)
-            .provideLayer(HttpClientZioBackend.layer())
-            .run(ZSink.collectAll)
-            .map(it => Response.json(it.toVector.map(_.raw.toString()).toString()))
+            .execute(req).provide(
+            ElasticExecutor.local,
+            Elasticsearch.layer,
+            HttpClientZioBackend.layer()
+          ).aggregation("asin").map { it => Response.json(it.map(_.toString()).toString()) }
             .mapError { it => responseError(it.getMessage) }
         } yield res
     }
